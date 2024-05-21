800e90f6e0b7f38b0b3d310dd4b520fd2088f6dd
Ethan <ethanhuyx@gmail.com>
2024-05-21 12:06:45 +0800

    Initial Commit

---
+++ SST INC/SST_INCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SST_INCApp.swift
+//  SST INC
+//
+//  Created by HU YIXIANG ETHAN (SST) on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SST_INCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
