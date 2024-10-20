4af9d3a6ad69586d120f443f60c57c0a40c03319
DARKSTARx178 <ethanhuyx@gmail.com>
2024-10-20 20:59:52 +0800

    Draft for Incoin Heist quiz

---
+++ SST INC/FinalView.swift
@@ -0,0 +1,18 @@
+//
+//  FinalView.swift
+//  SST INC
+//
+//  Created by HU YIXIANG ETHAN (SST) on 25/05/2024.
+//
+
+import SwiftUI
+
+struct FinalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinalView()
+}
